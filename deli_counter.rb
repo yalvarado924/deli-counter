b72b7742bbc28f1e53f5458559f041563365dafd
Yuliana Alvarado <yalvarado924@gmail.com>
2020-11-11 16:53:41 -0600

    commit tree dc3062eb5cc9354525c9a5d9268a90390813aa30

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+    deli = []
+    if array.size == 0
+        puts "The line is currently empty."
+    else
+        array.each_with_index do |name,index|
+        deli.push("#{index + 1}. #{name}")
+        end
+    puts "The line is currently: #{deli.join(" ")}"
+    end
+end
+
+def take_a_number(katz_deli, name)
+        katz_deli.push(name)
+        puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(person)
+    if person.length == 0
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{person.shift}."
+    end
+end
